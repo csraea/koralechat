ed4e381d65d03c60cba5d6f248ced1c0d7747532
Oleksandr Korotetskyi <oleksandr.korotetskyi@student.tuke.sk>
2020-11-24 13:50:26 +0100

    K O R A L E - Header space added; - Search & dots icon added; - Added color scheme support and proper colo changes;

--- KorAleChat/constants/Colors.ts
+++ KorAleChat/constants/Colors.ts
@@ -1,5 +1,5 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = '#FA7268';
+const tintColorDark = '#FA7268';
 
 export default {
   light: {
--- KorAleChat/navigation/index.tsx
+++ KorAleChat/navigation/index.tsx
@@ -1,12 +1,16 @@
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
+import { ColorSchemeName, useColorScheme, View } from 'react-native';
+import Colors from '../constants/Colors';
 
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
 import BottomTabNavigator from './BottomTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
+
+import { MaterialCommunityIcons, Octicons } from "@expo/vector-icons"   // icon set for the earch icon
+
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
@@ -26,8 +30,36 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
+    <Stack.Navigator screenOptions={{
+      headerStyle: {
+        backgroundColor: Colors.light.tint
+      },
+      headerTitleStyle: {
+        fontWeight: "bold"
+      },
+      headerTitleAlign: "left"
+    }}>
+      <Stack.Screen
+        name="Root"
+        component={BottomTabNavigator} 
+        options={{
+          title: "K O R A L E",
+          headerRight: () => {
+            return <View  style={{ flexDirection: "row", justifyContent: "space-between", width: 60, marginRight: 6 }}>
+              <Octicons
+                name="search" 
+                size={24} 
+                color={useColorScheme() === 'dark' ? Colors.light.background : Colors.dark.background}/>
+              
+              <MaterialCommunityIcons
+                name="dots-vertical" 
+                size={24} 
+                color={useColorScheme() === 'dark' ? Colors.light.background : Colors.dark.background}/>
+            </View>;
+          }
+        }}
+      />
+
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
   );
